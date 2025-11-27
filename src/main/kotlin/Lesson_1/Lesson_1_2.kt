a353f4ac6573caccc8251a89e2e5b232daeb5099
vakolinko <vakolinko@gmail.com>
2025-11-27 20:34:07 +0400

    KS-1-2 create smth

---
+++ src/main/kotlin/Lesson_1/Lesson_1_2.kt
@@ -0,0 +1,13 @@
+package Lesson_1
+
+fun main() {
+    val numbersOfOrders: Int = 75
+    val thankYouVeryMuch: String = "Большое спасибо за покупку, приходите снова"
+    println(numbersOfOrders)
+    println(thankYouVeryMuch)
+    var numberOfEmployees: Short = 2000
+    //println(numberOfEmployees)
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+
+}
