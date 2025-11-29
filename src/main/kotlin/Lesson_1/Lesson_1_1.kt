8a049b76e8063dd54ddd65851e8ce7ad12fe384e
vakolinko <vakolinko@gmail.com>
2025-11-29 21:29:15 +0400

    KS-1-1 create smth

--- src/main/kotlin/Lesson_1/Lesson_1_1.kt
+++ src/main/kotlin/Lesson_1/Lesson_1_1.kt
@@ -2,5 +2,5 @@ package Lesson_1
 
 fun main() {
     val nambersOfOrders: Int = 75
-    val ThankForPurchase: String = "Большое спасибо за покупку, приходите снова"
+    val ThanksForPurchase: String = "Большое спасибо за покупку, приходите снова"
 }
