a8777d19f45945f50c21edce1cf71c9523142aca
vakolinko <vakolinko@gmail.com>
2025-11-29 21:21:19 +0400

    KS-1-2 create smth

--- src/main/kotlin/Lesson_1/Lesson_1_2.kt
+++ src/main/kotlin/Lesson_1/Lesson_1_2.kt
@@ -2,9 +2,9 @@ package Lesson_1
 
 fun main() {
     val numbersOfOrders: Int = 75
-    val thankForPurchase: String = "Большое спасибо за покупку, приходите снова"
+    val thanksForPurchase: String = "Большое спасибо за покупку, приходите снова"
     println(numbersOfOrders)
-    println(thankForPurchase)
+    println(thanksForPurchase)
     var numberOfEmployees: Short = 2000
     //println(numberOfEmployees)
     numberOfEmployees = 1999
