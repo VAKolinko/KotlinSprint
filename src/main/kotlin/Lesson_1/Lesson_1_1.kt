3931966931183bd88339fc5186fc8f7e38d643ac
vakolinko <vakolinko@gmail.com>
2025-11-30 21:08:37 +0400

    KS-1-1 corrected task

--- src/main/kotlin/Lesson_1/Lesson_1_1.kt
+++ src/main/kotlin/Lesson_1/Lesson_1_1.kt
@@ -2,5 +2,5 @@ package Lesson_1
 
 fun main() {
     val nambersOfOrders: Int = 75
-    val ThanksForPurchase: String = "Большое спасибо за покупку, приходите снова!"
+    val ThanksForPurchase: String = "Thanks for the purchase, come again"
 }
