e8ac40c7dbb891c3a7f3a63cc288c62d8a5b2594
vakolinko <vakolinko@gmail.com>
2025-11-30 16:32:34 +0400

    KS-1-5 corrected task

--- src/main/kotlin/Lesson_1/Lesson_1_5.kt
+++ src/main/kotlin/Lesson_1/Lesson_1_5.kt
@@ -1,13 +1,16 @@
 package Lesson_1
 
+const val constantlyTime = 60 // Значение секунд в одной минуте либо минут в одном часе
 fun main() {
-    val timeInSpace = 6480
-    val minutes = timeInSpace/60
-    val remainingSeconds = timeInSpace%60
+    val timeInSpace = 6480 //время в секундах
+    val minutes = timeInSpace / constantlyTime
+    val remainingSeconds = timeInSpace % constantlyTime
     println(remainingSeconds)
-    val hours = minutes/60
+    val hours = minutes / constantlyTime
     println(hours)
-    val remainingHours = minutes%60
-    println(remainingHours)
-    println("0$hours:${remainingHours}:0${remainingSeconds}")
+    val remainingMinutes = minutes % constantlyTime
+    println(remainingMinutes)
+    println(String.format("%02d:%02d:%02d", hours, remainingMinutes, remainingSeconds))
+
+
 }
