05b82859c3dce24555832905f8d1b848a60022a3
vakolinko <vakolinko@gmail.com>
2025-12-02 19:46:20 +0400

    KS-2-2 corrected task

--- src/main/kotlin/Lesson_2/Lesson_2_2.kt
+++ src/main/kotlin/Lesson_2/Lesson_2_2.kt
@@ -1,9 +1,13 @@
 package Lesson_2
 
 fun main() {
-    val expensesForPermanentEmployees = 30000*50
-    val generalExpenses = expensesForPermanentEmployees + 20000*30
-    val averageSalary = generalExpenses / (50+30)
+    val numberOfEmployees = 50
+    val employeeSalary = 30000
+    val numberOfInterns = 30
+    val internSalary = 20000
+    val expensesForPermanentEmployees = employeeSalary * numberOfEmployees
+    val generalExpenses = expensesForPermanentEmployees + internSalary * numberOfInterns
+    val averageSalary = generalExpenses / (numberOfEmployees + numberOfInterns)
     println(expensesForPermanentEmployees)
     println(generalExpenses)
     println(averageSalary)
