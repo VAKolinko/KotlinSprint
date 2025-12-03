41a85f9f73cbd8981d510fedd59569b546c2d2ce
vakolinko <vakolinko@gmail.com>
2025-12-03 18:59:34 +0400

    KS-2-5 corrected task

--- src/main/kotlin/Lesson_2/Lesson_2_5.kt
+++ src/main/kotlin/Lesson_2/Lesson_2_5.kt
@@ -2,12 +2,12 @@ package Lesson_2
 
 import kotlin.math.pow
 
-const val oneHundredPercent = 100
+const val ONE_HUNDRED_PERCENT = 100
 
 fun main() {
     val depositPeriod = 20
     val depositAmount = 70000
     val interestRate = 16.7
-    val totalAmount = depositAmount * ((oneHundredPercent + interestRate) / oneHundredPercent).pow(depositPeriod)
+    val totalAmount = depositAmount * ((ONE_HUNDRED_PERCENT + interestRate) / ONE_HUNDRED_PERCENT).pow(depositPeriod)
     println("%.3f".format(totalAmount))
 }
