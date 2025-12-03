f59c5e384d8da21632ef3365316fd5218045739f
vakolinko <vakolinko@gmail.com>
2025-12-03 20:15:47 +0400

    KS-3-2 create task

---
+++ src/main/kotlin/Lesson_3/Lesson_3_2.kt
@@ -0,0 +1,10 @@
+package Lesson_3
+
+fun main() {
+    val maidenFullName = "Андреева Татьяна Сергеевна"
+    val marriedFullName = "Сидорова Татьяна Сергеевна"
+    val maidenAge = 20
+    val marriedAge = 22
+    println("[$maidenFullName, $maidenAge]")
+    println("[$marriedFullName, $marriedAge]")
+}
