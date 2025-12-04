b425cd421f89d4ccb612ea2941f60edcca5fd519
vakolinko <vakolinko@gmail.com>
2025-12-04 19:56:14 +0400

    KS-3-2 correct task

--- src/main/kotlin/Lesson_3/Lesson_3_2.kt
+++ src/main/kotlin/Lesson_3/Lesson_3_2.kt
@@ -1,10 +1,12 @@
 package Lesson_3
 
 fun main() {
-    val maidenFullName = "Андреева Татьяна Сергеевна"
-    val marriedFullName = "Сидорова Татьяна Сергеевна"
-    val maidenAge = 20
-    val marriedAge = 22
-    println("[$maidenFullName, $maidenAge]")
-    println("[$marriedFullName, $marriedAge]")
+    val firstName = "Татьяна"
+    val patronymic = "Сергеевна"
+    var lastName = "Андреева"
+    var age = 20
+    println("[$lastName $firstName $patronymic, $age]")
+    age = 22
+    lastName = "Сидорова"
+    println("[$lastName $firstName $patronymic, $age]")
 }
